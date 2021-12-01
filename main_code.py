1bbcf4e0bd24b8cebf766d4b7618e94c4932ba79
Priyansh_Diwan <priyanshdiwan2@gmail.com>
2021-12-01 21:19:43 +0530

    upadte to tackle new messages

--- main_code.py
+++ main_code.py
@@ -51,14 +51,20 @@ def process_response(message):
         return "Ask no questions hear no lies"
     elif "morning" in str.lower(message):
         return "Good morning!"
-    elif "hello" or "hey" or "hi" in str.lower(message):
+    elif "mhm" in str.lower(message):
+        return "You seem bored! type:songs or poems"
+    elif "hello" in str.lower(message):
         return "Hey!"
+    elif str.lower(message)=="poem":
+        return "well you idiot you are talking to a chatbot i hope you understand the word CHATBOT!"
+    elif str.lower(message)=="song":
+        return "well you idiot you are talking to a chatbot i hope you understand the word CHATBOT!"
     elif "help" in str.lower(message):
         return "please call me and let me know"
     elif "fuck" in str.lower(message):
         return "i hope someday you devlop Self-cannibalistic traits and eat yourself to death!"
     else:
-        return "Hey! i am ASUS! Priyansh is not here Please call him if its urgent or he will reply you asap"
+        return "mhmm👀! i can't get you and Priyansh is not here, Please call him if its urgent or he will reply you asap! if you are bored type 'song' or 'poem'"
     
 
 #  CHECKING FOR NEW MESSAGES
@@ -67,17 +73,22 @@ def check_for_new_text():
     while True:#continuesly checks for new message 
         try:
              posi_of_greenmark=pt.locateOnScreen("whatsapp_autoresponse/green_dot.png", confidence=.8)#identifies new message tags 
-             if posi_of_greenmark is not None:
-                 pt.moveTo(posi_of_greenmark)
-                 pt.moveRel(-100,0)
-                 pt.click()#this helps you to open the new message
-                 post_response(process_response(get_message()))#these are the function to get message-process it and post the response
-                 sleep(1)#you can call this refresh rate adjust the value to toggle time between the program repling your new messages
-        except(Exception): 
-            print("no new messages")    
+             if posi_of_greenmark is not None:   
+                    pt.moveTo(posi_of_greenmark,duration=0.5)
+                    pt.moveRel(-100,0)
+                    pt.click()#this helps you to open the new message
+                    post_response(process_response(get_message()))#these are the function to get message-process it and post the response
+                    pt.moveTo(506,395,duration=0.25)
+                    pt.click()
+                    sleep(6)#you can call this refresh rate adjust the value to toggle time between the program repling your new messages
 
-        sleep(10)#you can call this refresh rate adjust the value to toggle the time after which it searches for new messages  
+        except(Exception): 
+            print("no new messages")         
+        sleep(6)#you can call this refresh rate adjust the value to toggle the time after which it searches for new messages  
      
 
-# RUNNIG THE CODE 
+        
 check_for_new_text()
+
+
+
