be2539cb223dfbc69d31c976decae1e8bbab2303
Priyansh_Diwan <priyanshdiwan2@gmail.com>
2021-12-01 18:59:01 +0530

    added new message checkin feature

--- main_code.py
+++ main_code.py
@@ -8,9 +8,9 @@ import pyperclip
 sleep(5)
 
 
-position1=pt.locateOnScreen("whatsapp_autoresponse/smile&paperclip.png",confidence=.8)
-x = position1[0]
-y = position1[1]
+# position1=pt.locateOnScreen("whatsapp_autoresponse/smile&paperclip.png",confidence=.8)
+# x = position1[0]
+# y = position1[1]
 
 
 # get message 
@@ -41,25 +41,43 @@ def post_response(message):
     pt.click()
     pt.typewrite(message,interval=0.01)
 
-    # pt.typewrite("\n",interval=0.1) UNCOMMENT IT TO SEND 
+    pt.typewrite("\n",interval=0.1) 
 
 
     
 #  PROCESS RESPONSE
 def process_response(message):
-    if "?" in str(message):
+    if "?" in str.lower(message):
         return "Ask no questions hear no lies"
-    elif "morning" in str(message):
+    elif "morning" in str.lower(message):
         return "Good morning!"
-    elif "hello" in str(message):
+    elif "hello" or "hey" or "hi" in str.lower(message):
         return "Hey!"
-    elif "help" in str(message):
+    elif "help" in str.lower(message):
         return "please call me and let me know"
+    elif "fuck" in str.lower(message):
+        return "i hope someday you devlop Self-cannibalistic traits and eat yourself to death!"
     else:
-        return "Hey! i am ASUS! Priyansh is not here Please call him if its urgent or hell reply you asap"
+        return "Hey! i am ASUS! Priyansh is not here Please call him if its urgent or he will reply you asap"
     
 
+#  CHECKING FOR NEW MESSAGES
+def check_for_new_text():
+        
+    while True:#continuesly checks for new message 
+        try:
+             posi_of_greenmark=pt.locateOnScreen("whatsapp_autoresponse/green_dot.png", confidence=.8)#identifies new message tags 
+             if posi_of_greenmark is not None:
+                 pt.moveTo(posi_of_greenmark)
+                 pt.moveRel(-100,0)
+                 pt.click()#this helps you to open the new message
+                 post_response(process_response(get_message()))#these are the function to get message-process it and post the response
+                 sleep(1)#you can call this refresh rate adjust the value to toggle time between the program repling your new messages
+        except(Exception): 
+            print("no new messages")    
 
+        sleep(10)#you can call this refresh rate adjust the value to toggle the time after which it searches for new messages  
+     
 
-# calling functions 
-post_response(process_response(get_message()))
+# RUNNIG THE CODE 
+check_for_new_text()
